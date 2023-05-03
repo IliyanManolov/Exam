e983060bcdfe85b4bc7200d94fcf0d35f6609364
IliyanManolov <killerood1@gmail.com>
2023-05-03 19:55:45 +0300

    enable/disable test + screenshots

--- testing_dynamic_controls.py
+++ testing_dynamic_controls.py
@@ -52,6 +52,42 @@ class TestAddRemoveElements(unittest.TestCase):
       self.assertTrue(checkbox_reappear)
 
 
+      input_field_appear = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="input-example"]/input')))
+      input_field_appear_page = "input_field_appear_page.png"
+      driver.save_screenshot(input_field_appear_page)
+      self.assertTrue(input_field_appear)
+
+      enable_button = driver.find_element(By.XPATH, '//*[@id="input-example"]/button')
+      enable_button_page = "enable_button_page.png"
+      driver.save_screenshot(enable_button_page)
+      self.assertTrue(enable_button)
+
+      enable_button.click()
+      disable_button_appear = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="input-example"]/button')))
+      disable_button_appear_page = "disable_button_appear_page.png"
+      driver.save_screenshot(disable_button_appear_page)
+      self.assertTrue(disable_button_appear)
+
+      disable_button = driver.find_element(By.XPATH, '//*[@id="input-example"]/button')
+      disable_button.click()
+      disable_button_click_page = "disable_button_click_page.png"
+      driver.save_screenshot(disable_button_click_page)
+
+      time.sleep(10)
+
+      input_field = driver.find_element(By.XPATH, '//*[@id="input-example"]/input')
+      self.assertTrue(input_field)
+      input_field.send_keys("test")
+      input_field_test_page = "input_field_test_page.png"
+      driver.save_screenshot(input_field_test_page)
+
+
+
+
+
+       
+
+
 
 if __name__ == '__main__':
    unittest.main()
